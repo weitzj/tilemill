b4be5f4f444cb617ab4b4c5eacf755055cc52cc9
rsudekum <bobby@mapbox.com>
2015-02-13 10:34:39 -0800

    run as travis user

--- scripts/install-postgres.sh
+++ scripts/install-postgres.sh
@@ -38,13 +38,13 @@ postgres -k $PGHOST > postgres.log &
 sleep 2
 
 # set up postgres to know about local temp directory
-psql postgres -c "CREATE TABLESPACE temp_disk LOCATION '${PGTEMP_DIR}';"
-psql postgres -c "SET temp_tablespaces TO 'temp_disk';"
+sudo -H -u travis psql postgres -c "CREATE TABLESPACE temp_disk LOCATION '${PGTEMP_DIR}';"
+sudo -H -u travis psql postgres -c "SET temp_tablespaces TO 'temp_disk';"
 
 # create postgis enabled db
 ./mason_packages/.link/bin/createdb template_postgis -T postgres
-psql template_postgis -c "CREATE EXTENSION postgis;"
-psql template_postgis -c "SELECT PostGIS_Full_Version();"
+sudo -H -u travis psql template_postgis -c "CREATE EXTENSION postgis;"
+sudo -H -u travis psql template_postgis -c "SELECT PostGIS_Full_Version();"
 
 # stop db when you are done
 # you'll only need the 'source' below if you run this from a new terminal
