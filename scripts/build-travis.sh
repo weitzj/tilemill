0ced862d6cc9a71d1555cab130dd95ee6b3ad945
rsudekum <bobby@mapbox.com>
2015-03-27 14:57:24 -0700

    [publish atom]

--- scripts/build-travis.sh
+++ scripts/build-travis.sh
@@ -20,11 +20,9 @@ if [ $PLATFORM == "linux" ] && [ -n "$GITSHA" ]; then
 elif [ $PLATFORM == "darwin" ] && [ -n "$GITSHA" ]; then
     set -x
     echo "Publishing $GITSHA"
-    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-    unzip awscli-bundle.zip
-    ./awscli-bundle/install -b ~/bin/aws
-    echo $PATH | grep ~/bin
-    export PATH=~/bin:$PATH
+    curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+    sudo python get-pip.py
+    pip install awscli
 
     ./scripts/build-tilemill.sh "$GITSHA" darwin
 fi
